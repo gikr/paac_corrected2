16212606ccbd98a6fc95f8bb6c2582e0b6d612d5
gikr <gaini.ikram@gmail.com>
2018-10-11 22:58:00 +0300

    Update env_T_2.py

--- emulators/env_T_2.py
+++ emulators/env_T_2.py
@@ -139,7 +139,7 @@ def make_game(length_lab):
 
   #game = GAME_ART[reward_location]
   #game, length, reward_location = game_art_function(9, *length_lab, reward_location)
-  game, length, reward_location = game_art_function(9, *length_lab)
+  game, length, reward_location = game_art_function_2(9, *length_lab)
   scrolly_info = prefab_drapes.Scrolly.PatternInfo(
       game, STAR_ART, board_northwest_corner_mark='+',
       what_lies_beneath=MAZES_WHAT_LIES_BENEATH[0],
